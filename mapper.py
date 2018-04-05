b95ab6c25dd1181a97009717df9b03d0c3c3b74b
bhavikngala <bhavikna@buffalo.edu>
2018-04-05 12:36:46 -0400

    replaced stemming with lemmatizing, restructured code

--- mapper.py
+++ mapper.py
@@ -2,7 +2,7 @@
 
 '''
 1. removed stop words
-2. stemming of words
+2. lemmatizing of words
 3. combiner implementation for each line
 '''
 
@@ -10,27 +10,28 @@ import sys
 
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 import re
 
-# list of stop words
-stopwordsList = stopwords.words('english')
+def singleWordCountMapper():
+	# list of stop words
+	stopwordsList = stopwords.words('english')
 
-# object of stemmer class
-ps = PorterStemmer()
+	# lemmatizing object
+	lemmatizer = WordNetLemmatizer()
 
-# compile a pattern to check if word contains
-# .,!?') at its end'
-symbolPatternAtEnd = re.compile("[#\.,!?'\)\]\}%\":;><|-]*$")
-symbolPatternAtStart = re.compile("^[#\.,!?'\)\]\}%\":;><|-]*")
+	# compile a pattern to check if word contains
+	# .,!?') at its end'
+	symbolPatternAtEnd = re.compile("[#\.,!?'\)\]\}%\":;><|-]*$")
+	symbolPatternAtStart = re.compile("^[#\.,!?'\)\]\}%\":;><|-]*")
 
-# combiner dictionary
-# structure to imitate associative array
-combiner = {}
+	# combiner dictionary
+	# structure to imitate associative array
+	combiner = {}
 
-# iterate over all the lines in the stdin
-for line in sys.stdin:
+	# iterate over all the lines in the stdin
+	for line in sys.stdin:
 
 	# remove leading and trailing whitespaces
 	line = line.strip()
@@ -52,23 +53,26 @@ for line in sys.stdin:
 		if len(s) > 0:
 			word = word[len(s):]
 
-		# if the word is not in stop words list
+		# if the word is not in stop words list and not hyperlinks
 		if not word in stopwordsList and not word[:5] == 'https':
-			# stem the word
-			stemmedWord = ps.stem(word)
+			# lemmatize the word
+			wordLemma = lemmatizer.lemmatize(word)
 
 			# add word to combiner dictionary
 			# if word is already present in dictionary
 			# then increment the count
-			if stemmedWord in combiner:
-				count = combiner.get(stemmedWord) + 1
-				combiner.update({stemmedWord:count})
+			if wordLemma in combiner:
+				count = combiner.get(wordLemma) + 1
+				combiner.update({wordLemma:count})
 			# if word is not present in dictionary then
 			# add the word with count 1
 			else:
-				combiner.update({stemmedWord:1})
+				combiner.update({wordLemma:1})
 		
-# emit word counts from combiner
-for word in combiner.keys():
+	# emit word counts from combiner
+	for word in combiner.keys():
 	if len(word.strip()) > 0:
 		print '%s\t%s' % (word, str(combiner.get(word)))
+
+if __name__ == '__main__':
+	singleWordCountMapper()
