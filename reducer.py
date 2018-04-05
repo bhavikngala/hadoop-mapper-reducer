ce546baa4b496137288646a1b166a4075822cedc
bhavikngala <bhavikna@buffalo.edu>
2018-04-05 14:02:30 -0400

    changed output string format

--- reducer.py
+++ reducer.py
@@ -4,6 +4,8 @@ from operator import itemgetter
 import sys
 
 def singleWordCountReducer():
+	outputStringFormat = "{text:'%s', size:%s},"
+
 	current_word = None
 	current_count = 0
 	word = None
@@ -34,7 +36,7 @@ def singleWordCountReducer():
 				# emit only if count exceeds 5
 				# this filters alot of words with count just 1
 				if current_count > 5:
-					print('%s\t%s' % (current_word, current_count))
+					print(outputStringFormats % (current_word, current_count))
 
 			current_count = count
 			current_word = word
@@ -43,7 +45,7 @@ def singleWordCountReducer():
 		# emit only if count exceeds 5
 		# this filters alot of words with count just 1
 		if current_count > 5:
-			print('%s\t%s' % (current_word, current_count))
+			print(outputStringFormat % (current_word, current_count))
 
 if __name__ == '__main__':
 	singleWordCountReducer()
