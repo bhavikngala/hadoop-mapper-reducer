504ac55ae3c61961eea5af4d8b2d04c5d9700d69
bhavikngala <bhavikna@buffalo.edu>
2018-04-05 12:37:31 -0400

    emit counts greater than 5, code restructuring

--- reducer.py
+++ reducer.py
@@ -3,37 +3,47 @@
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
-word = None
+def singleWordCountReducer():
+	current_word = None
+	current_count = 0
+	word = None
 
-while True:
-#for line in sys.stdin:
-	line = sys.stdin.readline()
-	if not line:
-		break
+	while True:
+	#for line in sys.stdin:
+		line = sys.stdin.readline()
+		if not line:
+			break
 
-	f = open('/home/hadoop/Desktop/lab2/wordcoount/reduceInput.txt', 'wa')
-	f.write(line+'\n')
-	f.close()
+		f = open('/home/hadoop/Desktop/lab2/wordcoount/reduceInput.txt', 'wa')
+		f.write(line+'\n')
+		f.close()
 
-	line = line.strip()
+		line = line.strip()
 
-	word, count = line.split('\t')
+		word, count = line.split('\t')
 
-	try:
-		count = int(count)
-	except ValueError:
-		continue
+		try:
+			count = int(count)
+		except ValueError:
+			continue
+
+		if current_word == word:
+			current_count += count
+		else:
+			if current_word:
+				# emit only if count exceeds 5
+				# this filters alot of words with count just 1
+				if current_count > 5:
+					print('%s\t%s' % (current_word, current_count))
+
+			current_count = count
+			current_word = word
 
 	if current_word == word:
-		current_count += count
-	else:
-		if current_word:
+		# emit only if count exceeds 5
+		# this filters alot of words with count just 1
+		if current_count > 5:
 			print('%s\t%s' % (current_word, current_count))
 
-		current_count = count
-		current_word = word
-
-if current_word == word:
-	print('%s\t%s' % (current_word, current_count))
+if __name__ == '__main__':
+	singleWordCountReducer()
